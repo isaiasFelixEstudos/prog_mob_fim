94d8f238f2bcd1fb597594e76423528505049a8e
Isaias Félix - Estudante <isa.fel.mach.de.alc@gmail.com>
2023-06-18 21:55:27 -0400

    dfsdf

--- app_recipes/lib/main.dart
+++ app_recipes/lib/main.dart
@@ -5,6 +5,7 @@ import 'homeScreen.dart';
 import 'splash.dart';
 import 'paginas/login.dart';
 import 'paginas/perfil.dart';
+import 'paginas/cadastrarusuario.dart';
 
 void main() {
   runApp(GetMaterialApp(
@@ -15,6 +16,7 @@ void main() {
       GetPage(name: '/', page: () => HomeScreen()),
       GetPage(name: '/login', page: () => Login()),
       GetPage(name: '/areuser', page: () => UpdateCadUser()),
+      GetPage(name: '/cadusuario', page: () => CadUser()),
     ],
   ));
 }
